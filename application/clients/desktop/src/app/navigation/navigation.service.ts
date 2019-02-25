d671abd38e0176cb40d49f9a546dfec8a5f8c98b
tharanirajan <tharani10d@gmail.com>
2019-02-25 12:28:44 +0530

    config dumb for version added

---
+++ application/clients/desktop/src/app/navigation/navigation.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SharedService } from '../../shared/shared.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+
+  constructor(
+    private http: HttpClient,
+    private sharedService: SharedService
+  ) { }
+
+  getVersion(name): Observable<any> {
+    return this.http.get(this.sharedService.configUrl + `/generation_flow/getbyname/`+name);
+  }
+
+  getBuildVersion(name): Observable<any> {
+    return this.http.get(this.sharedService.configUrl + `/generation_flow/getbyname/`+name);
+  }
+}
