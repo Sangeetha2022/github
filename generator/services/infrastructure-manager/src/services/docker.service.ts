4e12a0cfd0c1bd0e15f14daf79caea330a7815ef
Mithun <mithun.vikram@10decoders.in>
2019-07-23 20:54:11 +0530

    buildscript cloud added

--- generator/services/infrastructure-manager/src/services/docker.service.ts
+++ generator/services/infrastructure-manager/src/services/docker.service.ts
@@ -8,7 +8,7 @@ import { exec } from 'child_process';
 
 export class DockerService {
 
-    public generate_build_script_system_entry_pod(projectDetails, callback: CallableFunction) {
+    public generate_build_script_local(projectDetails, callback: CallableFunction) {
 
 
       
@@ -31,31 +31,49 @@ export class DockerService {
 
     }
 
-
-    public generate_build_script_app_pod(projectDetails, callback: CallableFunction) {
-
-
+       public generate_build_script_cloud(projectDetails, callback: CallableFunction) {
         
         let destination = projectDetails.destinationUrl + '/jenkins';
         let templatePath = projectDetails.templateUrl + '/docker';
-
-        let systemEntryCodePath = projectDetails.projectUrl + "/app/client/desktop";
 
         if (!fs.existsSync(destination)) {
             fs.mkdirSync(destination);
         }
 
-        //generate script app-pod-jenkins
-        let generateDockerScript = st.loadGroup(require(templatePath + '/jenkins_app_pod_script_stg'));
-        let dockerScript = generateDockerScript.render("jenkins_app_pod_script", [projectDetails.project_lowercase, systemEntryCodePath]);
-        fs.writeFile(destination + '/app-pod-jenkins.xml', dockerScript, function (err) {
+        //generate script cloud
+        let generateDockerScript = st.loadGroup(require(templatePath + '/build_script_cloud_stg'));
+        let dockerScript = generateDockerScript.render("build_script_cloud", [projectDetails.project_lowercase]);
+        fs.writeFile(destination + '/build_script_cloud.sh', dockerScript, function (err) {
             if (err) throw err;
-            console.log('app-pod-jenkins script generated!!')
-           
+            console.log('cloud-script generated!!')   
         })
-
-
     }
+
+
+    // public generate_build_script_app_pod(projectDetails, callback: CallableFunction) {
+
+
+        
+    //     let destination = projectDetails.destinationUrl + '/jenkins';
+    //     let templatePath = projectDetails.templateUrl + '/docker';
+
+    //     let systemEntryCodePath = projectDetails.projectUrl + "/app/client/desktop";
+
+    //     if (!fs.existsSync(destination)) {
+    //         fs.mkdirSync(destination);
+    //     }
+
+    //     //generate script app-pod-jenkins
+    //     let generateDockerScript = st.loadGroup(require(templatePath + '/jenkins_app_pod_script_stg'));
+    //     let dockerScript = generateDockerScript.render("jenkins_app_pod_script", [projectDetails.project_lowercase, systemEntryCodePath]);
+    //     fs.writeFile(destination + '/app-pod-jenkins.xml', dockerScript, function (err) {
+    //         if (err) throw err;
+    //         console.log('app-pod-jenkins script generated!!')
+           
+    //     })
+
+
+    // }
 
 
 
