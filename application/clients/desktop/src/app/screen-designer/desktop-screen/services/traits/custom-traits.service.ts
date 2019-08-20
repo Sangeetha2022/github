0c77c972b28499bdc0b31dfc8eeb0fc7aa420bc4
ramkumar-murugesan <ramkumarm201093@gmail.com>
2019-08-20 14:57:23 +0530

    unwanted code removed  and structurized in screen designer

---
+++ application/clients/desktop/src/app/screen-designer/desktop-screen/services/traits/custom-traits.service.ts
@@ -0,0 +1,194 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CustomTraitsService {
+
+    content(editor) {
+        editor.TraitManager.addType('content', {
+            events: {
+                'keyup': 'onChange',  // trigger parent onChange method on keyup
+            },
+
+            getInputEl: function () {
+                if (!this.inputEl) {
+                    const input = document.createElement('textarea');
+                    input.value = this.target.get('content');
+                    this.inputEl = input;
+                }
+                return this.inputEl;
+            },
+            onValueChange: function () {
+                this.target.set('content', this.model.get('value'));
+            }
+        });
+    }
+
+    flowsActionButton(editor) {
+        // action button add
+        editor.TraitManager.addType('actionButton', {
+            events: {
+                'click': function () {
+                    // console.log('print button clicked');
+                    const eventPopupModel = document.getElementById('EventPopup');
+                    // console.log('print eventPopupModel values are ------ ', eventPopupModel);
+                    eventPopupModel.style.display = 'block';
+                },
+            },
+            getInputEl() {
+                // tslint:disable-next-line:prefer-const
+                let button = <HTMLElement>document.createElement('button');
+                button.id = 'fieldButton';
+                button.style.width = '100%';
+                button.style.backgroundColor = '#4CAF50';
+                button.style.border = 'none';
+                button.style.color = 'white';
+                button.style.backgroundColor = '#008CBA';
+                button.style.fontSize = '12px !important';
+                button.style.cursor = 'pointer';
+                button.appendChild(document.createTextNode('Flow'));
+                return button;
+            },
+        });
+    }
+
+    entityFieldButton($this) {
+        // const $this = this;
+        $this.editor.TraitManager.addType('entityFieldButton', {
+            events: {
+                'click': function () {
+                    console.log('traits button before if --- ', this.target.changed['entity']);
+                    if (this.target.changed['entity'] !== undefined
+                        && this.target.changed['entity'] !== 'none') {
+                        $this.isFieldPopupModal = true;
+                        $this.ref.detectChanges();
+                        console.log('traits button after if --- ', $this.isFieldPopupModal);
+                    }
+                },
+            },
+            getInputEl() {
+                // tslint:disable-next-line:prefer-const
+                let button = <HTMLElement>document.createElement('button');
+                button.id = 'fieldButton';
+                button.style.width = '100%';
+                button.style.backgroundColor = '#4CAF50';
+                button.style.border = 'none';
+                button.style.color = 'white';
+                button.style.backgroundColor = '#008CBA';
+                button.style.fontSize = '12px !important';
+                button.style.cursor = 'pointer';
+                button.appendChild(document.createTextNode('Field'));
+                return button;
+            },
+        });
+    }
+
+    addGridRowButton(editor, columnOptions) {
+        // add button
+        editor.TraitManager.addType('addButton', {
+            events: {
+                'click': function () {
+                    const count = this.target.view.el.gridOptions.columnDefs.length + 1;
+                    const columnDefs = this.target.view.el.gridOptions.columnDefs;
+                    columnDefs.push({
+                        headerName: `column_${count}`,
+                        field: 'a',
+                        sortable: true,
+                        colId: `col${count}_id`
+                    });
+                    this.target.view.el.gridOptions.api.setColumnDefs(columnDefs);
+                    this.target.view.el.gridOptions.api.sizeColumnsToFit();
+                    columnOptions.push({ value: `col${count}_id`, name: `column_${count}` });
+                    const colTraits = this.target.get('traits').where({ name: 'colname' })[0];
+                    colTraits.set('options', columnOptions);
+                    editor.TraitManager.getTraitsViewer().render();
+                    // console.log('sessionStorage count are ', count, ' --- ', { value: `col${count}_id`, name: `column_${count}` });
+                    // const modal = <HTMLElement>document.querySelector('#agGridModal');
+                    // console.log('ag Grid modal are ----- ', modal);
+                    // if (selectedEntity !== undefined) {
+                    //   // modal.style.display = 'block';
+                    //   localDataService.setAgGridEntity(selectedEntity);
+                    // }
+                    // trigger when btn is clicked
+                },
+            },
+            getInputEl() {
+                const button = <HTMLElement>document.createElement('button');
+                button.id = 'addButton';
+                button.style.width = '100%';
+                button.style.backgroundColor = '#4CAF50';
+                button.style.border = 'none';
+                button.style.color = 'white';
+                button.style.backgroundColor = '#008CBA';
+                button.style.fontSize = '16px';
+                button.style.cursor = 'pointer';
+                button.appendChild(document.createTextNode('+'));
+                return button;
+            },
+        });
+    }
+
+    removeGridRowButton(editor, columnOptions) {
+        // remove button
+        editor.TraitManager.addType('removeButton', {
+            events: {
+                'click': function () {
+                    const columnDefs = this.target.view.el.gridOptions.columnDefs;
+                    columnDefs.pop();
+                    this.target.view.el.gridOptions.api.setColumnDefs(columnDefs);
+                    this.target.view.el.gridOptions.api.sizeColumnsToFit();
+                    columnOptions.pop();
+                    const colTraits = this.target.get('traits').where({ name: 'colname' })[0];
+                    colTraits.set('options', columnOptions);
+                    editor.TraitManager.getTraitsViewer().render();
+                },
+            },
+            getInputEl() {
+                const button = <HTMLElement>document.createElement('button');
+                button.id = 'removeButton';
+                button.style.width = '100%';
+                button.style.backgroundColor = 'rgba(186, 43, 0, 0.73)';
+                button.style.border = 'none';
+                button.style.color = 'white';
+                button.style.fontSize = '16px';
+                button.style.cursor = 'pointer';
+                button.appendChild(document.createTextNode('-'));
+                return button;
+            },
+        });
+    }
+
+    gridFieldButton(editor, $this, selectedEntity, columnOptions) {
+        editor.TraitManager.addType('fieldGridButton', {
+            events: {
+                'click': function () {
+                    const modal = <HTMLElement>document.querySelector('#agGridModal');
+                    if (selectedEntity !== undefined) {
+                        // modal.style.display = 'block';
+                        const constructObj = {
+                            entity: selectedEntity,
+                            defalutColumn: this.target.view.el.gridOptions.columnDefs,
+                            customColumn: columnOptions
+                        };
+                        $this.dataService.setAgGridEntity(constructObj);
+                    }
+                    // trigger when btn is clicked
+                },
+            },
+            getInputEl() {
+                const button = <HTMLElement>document.createElement('button');
+                button.id = 'fieldButton';
+                button.style.width = '100%';
+                button.style.backgroundColor = '#4CAF50';
+                button.style.border = 'none';
+                button.style.color = 'white';
+                button.style.backgroundColor = '#008CBA';
+                button.style.fontSize = '12px !important';
+                button.style.cursor = 'pointer';
+                button.appendChild(document.createTextNode('Field'));
+                return button;
+            },
+        });
+    }
+}
