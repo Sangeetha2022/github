ae62739c90a9b608cd5236ef330fd95fe4d1d829
Mithun <mithun.vikram@10decoders.in>
2019-08-30 13:01:23 +0530

    configuration changes for stage env.

--- generator/services/github-manager/src/config/SharedService.ts
+++ generator/services/github-manager/src/config/SharedService.ts
@@ -1,7 +1,7 @@
 
 export class SharedService {
     // public static apiGatewayURL: String = `http://localhost:3000`;
-    public static systementryBaseUrl = "http://gep-dev-system-entry.gep-dev-201902.svc.cluster.local";
+    public static systementryBaseUrl = "http://gep-stage-system-entry.gep-stage-201908.svc.cluster.local";
 
     public static apiGatewayURL: String = SharedService.systementryBaseUrl + ":3000";
 }
