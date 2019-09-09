4aac4848cad7d8a58c68522441273a418b33cb2c
Udhaya <udhayakumar.m@10dcoders.in>
2019-09-09 15:57:15 +0530

    menu communicate via apigateway

--- application/clients/desktop/src/app/menu-builder/menu-builder.component.ts
+++ application/clients/desktop/src/app/menu-builder/menu-builder.component.ts
@@ -42,52 +42,12 @@ export class MenuBuilderComponent implements OnInit {
   private menuBuilder: any;
   private dataMenu: any;
 
-  // state: ITreeState = {
-  //   expandedNodeIds: {
-  //     1: true,
-  //     2: true
-  //   },
-  //   hiddenNodeIds: {},
-  //   activeNodeIds: {}
-  // };
-
-  // options: ITreeOptions = {
-  //   allowDrag: (node) => node.isLeaf,
-  //   getNodeClone: (node) => ({
-  //     ...node.data,
-  //     id: v4(),
-  //     name: `copy of ${node.data.name}`
-  //   })
-  // };
-
-  // nodes = [
-  //   {
-  //     id: 1,
-  //     name: 'root1',
-  //     children: [
-  //       { name: 'child1' },
-  //       { name: 'child2' }
-  //     ]
-  //   },
-  //   {
-  //     name: 'root2',
-  //     id: 2,
-  //     children: [
-  //       { name: 'child2.1', children: [] },
-  //       { name: 'child2.2', children: [
-  //         {name: 'grandchild2.2.1'}
-  //       ] }
-  //     ]
-  //   },
-  //   { name: 'root3' },
-  //   { name: 'root4', children: [] },
-  //   { name: 'root5', children: null }
-  // ];
   constructor(
     private dataService: DataService,
     private database: TreeDragService,
     private route: ActivatedRoute,
     private menuBuilderService: MenuBuilderService,
+    private projectComp: EntityManagerComponent,
     private screenService: ScreenDesignerService,
     private projectComponentService: ProjectComponentService,
   ) {
@@ -126,113 +86,15 @@ export class MenuBuilderComponent implements OnInit {
     );
   }
 
-  updateDiffInMenu() {
-    this.menuBuilderDetails.forEach(mData => {
-      this.languages.forEach(language => {
-        if (language === mData.language && mData.menu_option === true) {
-          this.newMenu = mData;
-        } else if (language === mData.language && mData.menu_option === false) {
-          this.oldMenu = mData;
-        }
-        let FeatureDiff = [];
-        if (this.newMenu.feature !== undefined && this.oldMenu.feature !== undefined) {
-          FeatureDiff = this.oldMenu.feature
-            .filter(x => !this.newMenu.feature.includes(x));
-          if (FeatureDiff.length > 0) {
-            const array = [];
-            FeatureDiff.forEach(featureId => {
-              if (!mData.feature.includes(featureId)) {
-                mData.feature.push(featureId);
-                this.featureDetailsData = [];
-                this.projectComponentService.getFeatureById(featureId).subscribe(
-                  feature => {
-                    this.featureDetailsData = feature;
-                    this.menuFId = this.featureDetailsData._id;
-                    this.menuFName = this.featureDetailsData.name;
-                    const fMenuData = {
-                      feature: this.menuFName,
-                      featureId: this.menuFId,
-                    };
-                    this.screenService.getScreenByFeatureId(featureId).subscribe(data => {
-                      if (data.length !== 0) {
-                        this.screenMenuName = [];
-                        this.screenId = [];
-                        data.forEach(sData => {
-                          this.screenId.push(sData._id);
-                          this.screenMenuName.push(sData.screenName);
-                        });
-                        const screenData = {
-                          screen: this.screenMenuName,
-                          screenId: this.screenId
-                        };
-                        const obj = {
-                          featuremenu: [{ name: fMenuData, description: fMenuData }],
-                          screenmenu: [{
-                            name: screenData,
-                            description: screenData
-                          }],
-                        };
-                        array.push(obj);
-                        this.menuBuilder = mData;
-                        this.menuBuilder.menuDetails = array;
-                        if (this.menuDetails.length !== 0) {
-                          this.menuDetails.forEach(meData => {
-                            this.menuBuilder.menuDetails.forEach(menu => {
-                              if (meData.featuremenu.length > 0) {
-                                if (menu.featuremenu[0].name.featureId === meData.featuremenu[0].name.featureId) {
-                                  menu.featuremenu[0].description = meData.featuremenu[0].description;
-                                  if (menu.screenmenu[0].name.screenId !== undefined && meData.screenmenu[0].name.screenId !== undefined) {
-                                    const intersection = menu.screenmenu[0].name.screenId.filter(x => meData.screenmenu[0].name.screenId.includes(x));
-                                    if (intersection.length !== 0) {
-                                      intersection.forEach(sId => {
-                                        meData.screenmenu[0].name.screenId.forEach((dSId, index) => {
-                                          if (sId === dSId) {
-                                            menu.screenmenu[0].description.screen[index] = meData.screenmenu[0].description.screen[index];
-                                          }
-                                        });
-                                      });
-                                    }
-                                  }
-                                }
-                              }
-                            });
-                          });
-                          if (this.menuBuilder.menuDetails[0].featuremenu[0].name.feature !== 'default') {
-                            this.menuBuilder.menuDetails.splice(0, 0, this.menuDetails[0]);
-                          }
-                        }
-                        this.menuBuilderService.updateMenuById(mData._id, this.menuBuilder)
-                          .subscribe(fMenu => {
-                            if (fMenu) {
-                              this.database.initialize(fMenu.menuDetails);
-                            }
-                          });
-                      }
-                    });
-
-                  },
-                  error => {
-
-                  }
-                );
-              }
-            });
-            this.menuDetails = mData.menuDetails;
-            this.updateMenuById(mData._id, mData);
-            this.database.initialize(mData.menuDetails);
-          }
-        }
-      });
-    });
-  }
-
   getMenuByProjectId() {
-    this.menuBuilderService.getMenuBuilderByProjectId(this.project_id).subscribe(response => {
-      const menuBuilderData = response.body;
+    this.menuBuilderService.getMenuBuilderByProjectId(this.project_id).subscribe(menuBuilderData => {
+      this.menuBuilderDetails = menuBuilderData;
+      const array = [];
       if (menuBuilderData.length !== 0) {
-        menuBuilderData.forEach(mData => {
+        menuBuilderData.body.forEach(mData => {
           if (!this.changeMenu) {
             if (mData.menu_option === true) {
+              console.log('----------------', mData);
               this.languages = mData.project_languages;
               this.selectedLang = mData.language;
               this.currentLang = mData.language;
@@ -247,74 +109,74 @@ export class MenuBuilderComponent implements OnInit {
               mData.menu_option = true;
               this.updateMenuById(mData._id, mData);
             }
-
-            if (meData.language !== this.selectedLang) {
-              meData.menu_option = false;
-              this.updateMenuById(meData._id, meData);
-            } else if (meData.language === this.selectedLang) {
-              meData.menu_option = true;
-              meData.language = this.selectedLang;
-              let FeatureDiff;
-              if (this.newMenu.feature !== undefined && this.oldMenu.feature !== undefined) {
-                FeatureDiff = this.oldMenu.feature
-                  .filter(x => !this.newMenu.feature.includes(x));
-                if (FeatureDiff.length > 0) {
-                  const array = [];
-                  FeatureDiff.forEach(featureId => {
-                    if (!meData.feature.includes(featureId)) {
-                      meData.feature.push(featureId);
-                      this.featureDetailsData = [];
-                      this.projectComponentService.getFeatureById(featureId).subscribe(
-                        feature => {
-                          this.featureDetailsData = feature.body;
-                          this.menuFId = this.featureDetailsData._id;
-                          this.menuFName = this.featureDetailsData.name;
-                          const fMenuData = {
-                            feature: this.menuFName,
-                            featureId: this.menuFId,
-                          };
-                          this.screenService.getScreenByFeatureId(featureId).subscribe(data => {
-                            if (data.body && data.body.length !== 0) {
-                              this.screenMenuName = [];
-                              this.screenId = [];
-                              data.body.forEach(sData => {
-                                this.screenId.push(sData._id);
-                                this.screenMenuName.push(sData.screenName);
-                              });
-                              const screenData = {
-                                screen: this.screenMenuName,
-                                screenId: this.screenId
+            this.languages.forEach(language => {
+              if (language === mData.language && mData.menu_option === true) {
+                this.newMenu = mData;
+              } else if (language === mData.language && mData.menu_option === false) {
+                this.oldMenu = mData;
+              }
+              if (language !== this.currentLang) {
+                let FeatureDiff = [];
+                if (this.newMenu.feature !== undefined && this.oldMenu.feature !== undefined) {
+                  FeatureDiff = this.oldMenu.feature
+                    .filter(x => !this.newMenu.feature.includes(x));
+                  if (FeatureDiff.length > 0) {
+                    FeatureDiff.forEach(featureId => {
+                      this.newMenu.feature.push(featureId);
+                    });
+                    if (this.newMenu.feature.length > 0) {
+                      this.newMenu.feature.forEach(feData => {
+                        if (feData !== null) {
+                          this.featureDetailsData = [];
+                          this.projectComponentService.getFeatureById(feData).subscribe(
+                            feature => {
+                              this.featureDetailsData = feature.body;
+                              this.menuFId = this.featureDetailsData._id;
+                              this.menuFName = this.featureDetailsData.name;
+                              const fMenuData = {
+                                feature: this.menuFName,
+                                featureId: this.menuFId,
                               };
-                              const obj = {
-                                featuremenu: [{ name: fMenuData, description: fMenuData }],
-                                screenmenu: [{
-                                  name: screenData,
-                                  description: screenData
-                                }],
-                              };
-                              array.push(obj);
-                              this.menuBuilder = meData;
-                              this.menuBuilder.menuDetails = array;
-                              if (this.dataMenu.length !== 0) {
-                                // tslint:disable-next-line:no-shadowed-variable
-                                this.dataMenu.forEach(meData => {
-                                  this.menuBuilder.menuDetails.forEach(menu => {
-                                    if (meData.featuremenu.length > 0) {
-                                      if (menu.featuremenu[0].name.featureId === meData.featuremenu[0].name.featureId) {
-                                        menu.featuremenu[0].description = meData.featuremenu[0].description;
-                                        // tslint:disable-next-line:max-line-length
-                                        if (menu.screenmenu[0].name.screenId !== undefined && meData.screenmenu[0].name.screenId !== undefined) {
-                                          // tslint:disable-next-line:max-line-length
-                                          const intersection = menu.screenmenu[0].name.screenId.filter(x => meData.screenmenu[0].name.screenId.includes(x));
-                                          if (intersection.length !== 0) {
-                                            intersection.forEach(sId => {
-                                              meData.screenmenu[0].name.screenId.forEach((dSId, index) => {
-                                                if (sId === dSId) {
-                                                  // tslint:disable-next-line:max-line-length
-                                                  menu.screenmenu[0].description.screen[index] = meData.screenmenu[0].description.screen[index];
-                                                }
-                                              });
-                                            });
+                              this.screenService.getScreenByFeatureId(feData).subscribe(data => {
+                                if (data.length !== 0) {
+                                  this.screenMenuName = [];
+                                  this.screenId = [];
+                                  data.body.forEach(sData => {
+                                    this.screenId.push(sData._id);
+                                    this.screenMenuName.push(sData.screenName);
+                                  });
+                                  const screenData = {
+                                    screen: this.screenMenuName,
+                                    screenId: this.screenId
+                                  };
+                                  const obj = {
+                                    featuremenu: [{ name: fMenuData, description: fMenuData }],
+                                    screenmenu: [{
+                                      name: screenData,
+                                      description: screenData
+                                    }],
+                                  };
+                                  array.push(obj);
+                                  this.menuBuilder = this.newMenu;
+                                  this.menuBuilder.menuDetails = array;
+                                  if (this.menu.length !== 0) {
+                                    this.menu.forEach(meData => {
+                                      this.menuBuilder.menuDetails.forEach(menu => {
+                                        if (meData.featuremenu.length > 0) {
+                                          if (menu.featuremenu[0].name.featureId === meData.featuremenu[0].name.featureId) {
+                                            menu.featuremenu[0].description = meData.featuremenu[0].description;
+                                            if (menu.screenmenu[0].name.screenId !== undefined && meData.screenmenu[0].name.screenId !== undefined) {
+                                              const intersection = menu.screenmenu[0].name.screenId.filter(x => meData.screenmenu[0].name.screenId.includes(x));
+                                              if (intersection.length !== 0) {
+                                                intersection.forEach(sId => {
+                                                  meData.screenmenu[0].name.screenId.forEach((dSId, index) => {
+                                                    if (sId === dSId) {
+                                                      menu.screenmenu[0].description.screen[index] = meData.screenmenu[0].description.screen[index];
+                                                    }
+                                                  });
+                                                });
+                                              }
+                                            }
                                           }
                                         }
                                       });
@@ -325,15 +187,7 @@ export class MenuBuilderComponent implements OnInit {
                                   }
                                   this.updateMenuById(this.menuBuilder._id, this.menuBuilder);
                                 }
-                              }
-                              this.menuBuilderService.updateMenuById(meData._id, this.menuBuilder)
-                                .subscribe(fMenu => {
-                                  if (fMenu.body) {
-                                    this.database.initialize(fMenu.body.menuDetails);
-                                  }
-                                });
-                            }
-                          });
+                              });
 
                             },
                             error => {
@@ -343,55 +197,18 @@ export class MenuBuilderComponent implements OnInit {
                         }
                       });
                     }
-                    // this.updateMenuById(this.newMenu._id, this.newMenu);
                   }
                 }
               }
-              // this.updateDiffInMenu();
             });
           }
         });
       }
     });
   }
-
-  updateMenuBuilder(description) {
-    this.menuDetails.forEach(element => {
-      if (element.featuremenu[0].description.feature === this.descriptionBeforeUpdate) {
-        element.featuremenu[0].description.feature = description;
-      } else {
-        element.screenmenu.forEach(sData => {
-          sData.description.screen.forEach((screen, index) => {
-            if (screen === this.descriptionBeforeUpdate) {
-              sData.description.screen.splice(index, 1, description);
-            }
-          });
-        });
-      }
-    });
-    this.updateMenuById(this.currentMenuDetails._id, this.currentMenuDetails);
-
-    // this.menuLang.forEach(lang => {
-    //   if (this.secondaryLang !== undefined) {
-    //     if (lang !== this.secondaryLang) {
-    //       this.menuBuilderDetails.language = this.primaryLang;
-    //       this.menuBuilderDetails.menu_option = false;
-    //       this.updateMenuById(this.menuBuilderDetails._id, this.menuBuilderDetails);
-    //       delete this.menuBuilderDetails._id;
-    //       delete this.menuBuilderDetails.updated_date;
-    //       delete this.menuBuilderDetails.created_date;
-    //       this.menuBuilderDetails.menu_option = true;
-    //       this.menuBuilderDetails.language = this.selectedLang;
-    //     }
-    //   } else if (lang === this.primaryLang) {
-    //     this.database.initialize(this.menuBuilderDetails.menuDetails);
-    //     this.updateMenuById(this.menuBuilderDetails._id, this.menuBuilderDetails);
-    //   }
-    // });
-  }
   updateMenuById(id, menu) {
     this.menuBuilderService.updateMenuById(id, menu).subscribe(fMenu => {
-      this.database.initialize(fMenu.menuDetails);
+      this.database.initialize(fMenu.body.menuDetails);
       this.name = '';
       this.description = '';
     });
@@ -401,7 +218,6 @@ export class MenuBuilderComponent implements OnInit {
     if (this.currentLang !== this.selectedLang) {
       this.changeMenu = true;
       this.currentLang = this.selectedLang;
-      // console.log('-------------------', this.currentMenuDetails)
       this.currentMenuDetails.menu_option = false;
       this.updateMenuById(this.currentMenuDetails._id, this.currentMenuDetails);
       this.getMenuByProjectId();
