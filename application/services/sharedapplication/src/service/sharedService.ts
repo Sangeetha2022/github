cbefd813d6f289fc046203b7a78ac231c2179432
Mithun <mithun.vikram@10decoders.in>
2020-04-08 14:10:20 +0530

    exceptions handled

--- application/services/sharedapplication/src/service/sharedService.ts
+++ application/services/sharedapplication/src/service/sharedService.ts
@@ -20,18 +20,12 @@ export class sharedService {
         console.log("projectId------->", projectId)
         let overallprojectdetails = await this.resolveProjectEntities(projectId);
         let projectDetails = overallprojectdetails['projectdetails'];
+        console.log("projectDetails----->", projectDetails);
         let projectEntities = overallprojectdetails['projectentity'];
         let projectFeatures = overallprojectdetails['projectfeature']
         // console.log("overallproject---->", overallprojectdetails);
-        console.log("projectEntities----->", projectEntities);
+        // console.log("projectEntities----->", projectEntities);
             projectEntities.body.forEach(function (entity) {
-                // if(entity.feature_id) {
-                //     projectFeatures.body.forEach(function (feature){
-                //         if(entity.feature_id == feature._id){
-                //             // feature.entities.push({entityName: entity.name})
-                //         }
-                //     })
-                // }
                 entity.field.forEach(function (details) {
                     delete entity.__v;
                     delete entity.is_default;
@@ -73,17 +67,18 @@ export class sharedService {
                 projectTemplateName: projectDetails.body.app_ui_template_name,
                 primaryLanguage: projectDetails.body.default_human_language,
                 secondaryLanguages: projectDetails.body.other_human_languages,
-                clientlanguage: projectDetails.body.clientlanguage.name,
-                clientframework: projectDetails.body.clientframework.name,
-                serverlanguage: projectDetails.body.serverlanguage.name,
-                serverframework: projectDetails.body.serverframework.name,
-                serverdatabase: projectDetails.body.serverdatabase.name,
-                servertarget: projectDetails.body.servertarget.name,
-                server_deployment_type: projectDetails.body.server_deployment_type.name,
-                User_Id: projectDetails.body.UserId,
+                clientlanguage: projectDetails.body.clientlanguage ? projectDetails.body.clientlanguage.name : null,
+                clientframework: projectDetails.body.clientframework ? projectDetails.body.clientframework.name : null,
+                serverlanguage: projectDetails.body.serverlanguage ? projectDetails.body.serverlanguage.name : null,
+                serverframework: projectDetails.body.serverframework ? projectDetails.body.serverframework.name : null,
+                serverdatabase: projectDetails.body.serverdatabase ? projectDetails.body.serverdatabase.name : null,
+                servertarget: projectDetails.body.servertarget ? projectDetails.body.servertarget.name : null,
+                server_deployment_type: projectDetails.body.server_deployment_type ? projectDetails.body.server_deployment_type.name : null,
                 projectEntities: projectEntities.body,
                 projectFeatures: projectFeatures.body,
             }
+
+            // console.log("newObject", newObject);
             let yamlStr = yaml.dump(newObject);
             fs.writeFileSync(projectDetails.body.name.concat(".yaml"), yamlStr, 'utf8');
             callback('Yaml file convertion is done');
