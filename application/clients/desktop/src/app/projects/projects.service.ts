026beeda9af202cb30e82790681f8a886fbac2ea
Mithun <mithun.vikram@10decoders.in>
2020-04-20 13:28:45 +0530

    fixed the file upload

--- application/clients/desktop/src/app/projects/projects.service.ts
+++ application/clients/desktop/src/app/projects/projects.service.ts
@@ -99,17 +99,13 @@ export class ProjectsService {
   importSharedServiceYaml(fileToUpload: File,user_id): Observable<any> {
     const endpoint = `${this.restapi.Apigateway}${Constants.sharedAppImport}/${user_id}`;
     const formData: FormData = new FormData();
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //       'Content-Type':  'multipart/form-data',
-    //   })
-    //   };
     formData.append('fileKey', fileToUpload, fileToUpload.name);
-    // let headers = new Headers();
-    //     headers.set('Accept', 'multipart/form-data');
     console.log("formdata",formData)
     console.log("filetoupload",fileToUpload)
-    // return this.api.post(`${this.restapi.Apigateway}${Constants.sharedAppI,mport}`)
-    return this.api.post(endpoint, formData,);
+    fetch(endpoint + 'fileKey', {
+      method: 'POST',
+      body: formData
+    })
+    return this.api.post(endpoint,formData);
 }
 }
