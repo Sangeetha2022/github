67bde5e3eedcf87a95fbab7ccd993d0782fc57d1
Mithun <mithun.vikram@10decoders.in>
2020-07-08 16:11:58 +0530

    wcmx feature changes

--- generator/services/backend-gen-manager/src/services/apiGatewayService.ts
+++ generator/services/backend-gen-manager/src/services/apiGatewayService.ts
@@ -1,5 +1,6 @@
 import { Request } from 'mongoose';
 import * as fs from 'fs';
+import * as ncp from 'ncp';
 import { SharedService } from '../config/SharedService';
 import { ApiAdaptar } from '../config/ApiAdaptar';
 import { MicroFlowManagerService } from '../apiservices/MicroFlowManagerService';
@@ -14,8 +15,45 @@ export class ApiGatewayService {
 
 
     public async createApiGateway(req: Request, callback: CallableFunction) {
+        var projectgenpath = req.body.projectGenerationPath;
+        var seedpath = req.body.project.templateLocation.authTemplatePath;
+        console.log("req-------->projectGenerationPath", req.body.projectGenerationPath);
+        console.log("req-------->authTemplatePath", req.body.project.templateLocation.authTemplatePath);
+
+        var WCMX_ACOUSTICPATH = `${projectgenpath}/wcmx-acoustic`;
+        var CDNMANAGER_PATH = `${projectgenpath}/cdn-manager`;
+        var SEED_ACOUSTICPATH = `${seedpath}/wcmx-acoustic`;
+        var SEED_CDNMANAGERPATH = `${seedpath}/cdn-manager`;
+
+        console.log("SEED_CDNMANAGERPATH", SEED_CDNMANAGERPATH);
+        if (!fs.existsSync(projectgenpath)) {
+            fs.mkdirSync(projectgenpath);
+        }
+        if (!fs.existsSync(WCMX_ACOUSTICPATH)) {
+            console.log('-----coming here for acoustic creation---', WCMX_ACOUSTICPATH);
+            fs.mkdirSync(WCMX_ACOUSTICPATH);
+        }
+        if (!fs.existsSync(CDNMANAGER_PATH)) {
+            console.log('-----coming here for cdnmanager creation---', CDNMANAGER_PATH);
+            fs.mkdirSync(CDNMANAGER_PATH);
+        }
+        //copy template files for acoustic
+        ncp.limit = 16;
+        ncp(SEED_ACOUSTICPATH, WCMX_ACOUSTICPATH, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('acoustic code generated.....');
+        });
+        ncp(SEED_CDNMANAGERPATH, CDNMANAGER_PATH, (err) => {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('cdnamanger code generated.....');
+        });
+
         const apiGateway = await this.generateApiGateway(req.body);
-        console.log('backend services apigateway ----@@@@@@@@@@@@@@@@@@@@@@-   ', apiGateway);
+        console.log('backend services apigateway ----@@-   ', apiGateway);
         callback('apigateway generated')
     }
 
--- generator/services/backend-gen-manager/src/services/backendAdminService.ts
+++ generator/services/backend-gen-manager/src/services/backendAdminService.ts
@@ -19,39 +19,6 @@ export class BackendAdminService {
         var projectgenpath = req.body.projectgenpath;
         var seedpath = req.body.seed;
 
-        var SOURCE_PATH = projectId;
-
-        var WCMX_ACOUSTICPATH = `${projectgenpath}/wcmx-acoustic`;
-        var CDNMANAGER_PATH = `${projectgenpath}/cdn-manager`;
-        var SEED_ACOUSTICPATH = `${seedpath}/wcmx-acoustic`;
-        var SEED_CDNMANAGERPATH = `${seedpath}/cdn-manager`;
-
-        console.log("SEED_CDNMANAGERPATH", SEED_CDNMANAGERPATH);
-
-            if (!fs.existsSync(WCMX_ACOUSTICPATH)) {
-                console.log('-----coming here for acoustic creation---', WCMX_ACOUSTICPATH);
-                fs.mkdirSync(WCMX_ACOUSTICPATH);
-            }
-            if (!fs.existsSync(CDNMANAGER_PATH)) {
-                console.log('-----coming here for cdnmanager creation---', CDNMANAGER_PATH);
-                fs.mkdirSync(CDNMANAGER_PATH);
-            }
-            //copy template files for acoustic
-            ncp.limit = 16;
-            ncp(SEED_ACOUSTICPATH, WCMX_ACOUSTICPATH, (err) => {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log('acoustic code generated.....');
-            });
-            ncp(SEED_CDNMANAGERPATH, CDNMANAGER_PATH, (err) => {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log('cdnamanger code generated.....');
-            });
-        
-
         this.adminmanagerservice.getAdmin(feature, projectId, projectgenpath,seedpath, (data) => {
             callback(data);
         })
