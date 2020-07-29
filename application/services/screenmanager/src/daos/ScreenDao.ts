1f9357981d2a297e1f0cc8abf3a2d49519735938
KishanR <kishan.r10decoders@gmail.com>
2020-07-29 15:01:17 +0530

    Fix for the duplicate css issue issue #410

--- application/services/screenmanager/src/daos/ScreenDao.ts
+++ application/services/screenmanager/src/daos/ScreenDao.ts
@@ -1,18 +1,23 @@
+/**The code changes which are made below inside a comment is that when we save the screens designer data grapesjs give us a default css 
+ * and we are saving them so upon generation the same gets duplicated so to avoid that I have handled that in backend side.
+ * To make any changes for that code in future please consult with Dan castillo or Kishan for more ref look at #410 in github.
+ */
 'use strict'
 import * as mongoose from 'mongoose';
 import { ScreenSchema } from '../models/Screen';
 import { FeatureDetailsSchema } from '../models/featuredetails.model';
 
 const screenModel = mongoose.model('screen', ScreenSchema);
-const featureDetailModel = mongoose.model('feature_details', FeatureDetailsSchema)
+const featureDetailModel = mongoose.model('feature_details', FeatureDetailsSchema);
+
 
 export class ScreenDao {
     constructor() { }
 
     public createScreen(screenData, callback) {
+        console.log('save template result ----- ', screenData);
         let template = new screenModel(screenData);
         template.save().then((result) => {
-            console.log('save template result ----- ', result);
             callback(result);
         }).catch((error) => {
             console.log('save template error -----  ', error);
@@ -42,9 +47,31 @@ export class ScreenDao {
     }
 
     public updateScreen(req, callback) {
-        console.log('update screen in dao --- ', req.params.id);
-        console.log('update screen in dao -body-- ', req.body);
-        screenModel.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
+        console.log('update screen in dao --- ', req.body);
+        let screendesigner = req.body;
+
+        /** Don't make any changes in the below code without consulting with Dan Castillo or Kishan
+         * For more reference check the issue number #410 July 29th 2020
+         * For the technical details see top
+         */
+        let defaultcss = new RegExp(/{box-sizing:border-box;}body{margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;}|\*/gi);
+        let duplicatevalue = screendesigner['gjs-css'].match(defaultcss);
+        let changedvalue  = [...new Set(duplicatevalue)];
+        let csschanges = changedvalue.join('');
+        const diff = (diffBy, diffMe) => diffBy.split(diffMe)
+        const C = diff(screendesigner['gjs-css'], csschanges);
+        csschanges += C;
+        let regex = /[,\s]/g;
+        csschanges = csschanges.replace(regex, '');
+
+        console.log('---------------the final css changes-----',csschanges);
+        screendesigner['gjs-css'] = csschanges;
+
+        /** Don't make any changes in the above code without consulting with Dan Castillo or Kishan
+         * For more refrence check the issue number #410 July 29th 2020
+         */
+
+        screenModel.findOneAndUpdate({ _id: req.params.id }, screendesigner , { new: true }, (err, result) => {
             if (err) {
                 console.log('err in update ---- ', err);
                 callback(err);
