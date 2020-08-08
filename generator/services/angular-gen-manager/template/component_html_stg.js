f2684af4494a1ebdc6d3966ff53297f3fd87b54f
TamilB <tamilarasan.b@10decoders.in>
2020-08-08 13:31:20 +0530

    Added h2 tag in generation application

--- generator/services/angular-gen-manager/template/component_html_stg.js
+++ generator/services/angular-gen-manager/template/component_html_stg.js
@@ -1,6 +1,6 @@
 /*
  * Template group component_html
- * Compiled on Wed Jul 31 2019 17:58:23 GMT+0530 (India Standard Time)
+ * Compiled on Sat Aug 08 2020 13:23:22 GMT+0530 (India Standard Time)
  */
 var path = require("path");
 var base = path.dirname(module.filename);
@@ -23,11 +23,15 @@ r = function(w, rc) {
     var g = this.owningGroup,
         s = this.scope;
     
-    if (st.test(st.prop(s, g, rc, s.object, "tag", { file: gFile, line: 2, column: 11 }))) {
+    w.write("<h2>");
+    st.write(w, s, g, rc, st.prop(s, g, rc, s.object, "className", { file: gFile, line: 2, column: 12 }));
+    w.write("</h2>");
+    w.write("\n");
+    if (st.test(st.prop(s, g, rc, s.object, "tag", { file: gFile, line: 3, column: 11 }))) {
     
         st.write(w, s, g, rc, (function() {
         var tp = [],
-        attr = st.prop(s, g, rc, s.object, "tag", { file: gFile, line: 2, column: 24 });
+        attr = st.prop(s, g, rc, s.object, "tag", { file: gFile, line: 3, column: 24 });
         tp.push(st.makeSubTemplate(g, function(w, rc) {
             var g = this.owningGroup,
             s = this.scope;
