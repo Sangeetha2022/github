874cdbb311b4d1605f3f8377fb23a7fc1103e50b
bjayaprakashcse <jayaprakash.10decoders@gmail.com>
2021-02-15 21:16:23 +0530

    Added the expandable popup model

--- generator/services/Angular-gen-ManagerV10/src/worker/componentservice/Flowserviceworker.ts
+++ generator/services/Angular-gen-ManagerV10/src/worker/componentservice/Flowserviceworker.ts
@@ -12,7 +12,7 @@ export class FlowServiceWorker {
         const flows = [];
         flows_info.forEach((flow: any) => {
             if (nodeResponse && nodeResponse.flowAction && nodeResponse.flowAction.length > 0) {
-                nodeResponse.flowAction.filter((e) => {
+                nodeResponse.flowAction.filter(async (e) => {
                     if (flow.flowName === e.methodName) {
                         if (e.routeUrl.includes('/:id')) {
                             e.routeUrl = e.routeUrl.replace('/:id', '');
@@ -23,25 +23,31 @@ export class FlowServiceWorker {
                             if(e.methodName === Constant.GP_CREATE_FLOW || e.methodName === Constant.GP_UPDATE_FLOW) {
                                 e['paramName'] = e.variableName;
                                 e['body'] = `return this.http.${e.apiAction}(this.sharedService.DESKTOP_API + '${e.routeUrl}', ${e['paramName']});`;
+                                flows.push(e);
                             }
                             // Mapping GpGetNounById and GpDelete Flow Actions Body
                             if(e.methodName === Constant.GP_GETNOUNBYID_FLOW || e.methodName === Constant.GP_DELETE_FLOW) {
                                 e['paramName'] = e.variableName + 'Id';
                                 e['body'] = `return this.http.${e.apiAction}(this.sharedService.DESKTOP_API + '${e.routeUrl}/' + ${e['paramName']});`;
+                                flows.push(e);
                             }
                             // Mapping GpGetAllValues Flow Action Body
                             if(e.methodName === Constant.GP_GETALLVALUES_FLOW) {
-                                desktopElement.entity_info.forEach(entity => {
+                                let flowActionObject:any = {};
+                                await desktopElement.entity_info.forEach(entity => {
                                     if(entity.htmlId === flow.htmlId) {
                                         let entites = details.entities.filter((x) => x._id === entity.entityId);
                                         let entityObject = entites[0];
-                                        e.routeUrl = e.routeUrl.replace(e.routeUrl, `/${entityObject.name}`);
-                                        e['body'] = `return this.http.${e.apiAction}(this.sharedService.DESKTOP_API + '${e.routeUrl}');`;
+                                        flowActionObject.apiAction = e.apiAction;
+                                        flowActionObject.variableName = entityObject.name;
+                                        flowActionObject.methodName = entityObject.name + Constant.GP_GETALLVALUES_FLOW;
+                                        flowActionObject.routeUrl = `/${entityObject.name}`;
+                                        flowActionObject['body'] = `return this.http.${flowActionObject.apiAction}(this.sharedService.DESKTOP_API + '${flowActionObject.routeUrl}');`;
                                     } else {
-                                        e['body'] = `return this.http.${e.apiAction}(this.sharedService.DESKTOP_API + '${e.routeUrl}');`;
+                                        flowActionObject['body'] = `return this.http.${flowActionObject.apiAction}(this.sharedService.DESKTOP_API + '${flowActionObject.routeUrl}');`;
                                     }
                                 })
-                                
+                                flows.push(flowActionObject);
                             }
                             // Mapping GpSearch Flow Action Body
                             if(e.methodName === Constant.GP_SEARCH_FLOW) {
@@ -49,8 +55,8 @@ export class FlowServiceWorker {
                                 e['body'] = gpSearchBody.join('\n \t \t');
                                 e['body'] = e['body'].replace('paramName', e.variableName);
                                 e['body'] = e['body'] + "\n \t \treturn this.http." + e.apiAction + "(this.sharedService.DESKTOP_API + `" + e.routeUrl + "${temp.length > 0 ? `?${temp.join('&')}` : ''}`);";
+                                flows.push(e);
                             }
-                            flows.push(e);
                         }
                     }
                 });
--- generator/services/Angular-gen-ManagerV10/src/worker/componentworker/Flowcomponentworker.ts
+++ generator/services/Angular-gen-ManagerV10/src/worker/componentworker/Flowcomponentworker.ts
@@ -1,5 +1,6 @@
 import { Constant } from '../../config/Constant';
 import { queryParamSubscribe } from '../../config/componentDependency';
+import { convertToObject } from 'typescript';
 
 export class FlowComponentWorker {
     /**
@@ -9,7 +10,7 @@ export class FlowComponentWorker {
      * @param entities 
      * Constructing the flows like GpCreate, GpUpdte, etc...
      */
-    constructFlowsInfo(desktopElement, flows_info: Array<Object>, nodeResponse: any, microflowObject: any, entities: any) {
+    constructFlowsInfo(details, desktopElement, flows_info: Array<Object>, nodeResponse: any, microflowObject: any, entities: any) {
         const flows = [];
         const gjs_components: any = JSON.parse(desktopElement['gjs-components'][0]);
         flows_info.forEach((flow: any) => {
@@ -71,23 +72,45 @@ export class FlowComponentWorker {
                         // Mapping the GpSearch and GpGetAllValues Flow Actions Body
                         if (flow.flowName === Constant.GP_SEARCH_FLOW || flow.flowName === Constant.GP_GETALLVALUES_FLOW) {
                             const variables = [];
+                            let entityName = '';
                             const flowAction = nodeResponse.flowAction.filter((item) => item.methodName === Constant.GP_SEARCH_FLOW || item.methodName === Constant.GP_GETALLVALUES_FLOW);
-                            if (flowAction && flowAction.length > 0) {
-                                if(desktopElement.is_grid_present === true) {
-                                    variables.push('this.rowData = data;');
-                                } else if (gjs_components.length > 0) {
-                                    gjs_components.forEach(gjs_element => {
-                                        if(gjs_element.components.length > 0) {
-                                            gjs_element.components.forEach(data => {
-                                                if(data.type === 'dynamicdropdown-type') {
-                                                    variables.push('this.itemArray = data;');
-                                                }
-                                            })
+                            desktopElement.entity_info.forEach(entity => {
+                                console.log('entity info details =======>>>', entity);
+                                if (entity.htmlId === flow.htmlId) {
+                                    let entites = details.entities.filter((x) => x._id === entity.entityId);
+                                    let entityObject = entites[0];
+                                    entityObject.field.forEach((fieldData) => {
+                                        if(fieldData._id === entity.fields.fieldId && fieldData.type_name === 'Entity') {
+                                            console.log('field object data ===========>>>', fieldData);
+                                            entityName = fieldData.entity_id.name;
+                                            temp.flowName = entityName + Constant.GP_GETALLVALUES_FLOW;
                                         }
-                                    })                                    
+                                    });
+                                    
+                                    
                                 }
-                                
-                            }
+                                if (flowAction && flowAction.length > 0) {
+                                    if (desktopElement.is_grid_present === true) {
+                                        variables.push('this.rowData = data;');
+                                    } else if (gjs_components.length > 0) {
+                                        gjs_components.forEach(gjs_element => {
+                                            if (gjs_element.components.length > 0) {
+                                                gjs_element.components.forEach(data => {
+                                                    console.log('data ===============>>>', data);
+                                                    if (data.type === 'dynamicdropdown-type' && entity.htmlId === data.attributes.id) {
+                                                        let entites = details.entities.filter((x) => x._id === entity.entityId);
+                                                        let entityObject = entites[0];
+                                                        if (entityName === entityObject.name) {
+                                                            variables.push(`this.${entityObject.name}itemArray = data;`);
+                                                        }
+                                                    }
+                                                })
+                                            }
+                                        })
+                                    }
+
+                                }
+                            })
                             if (variables.length > 0) {
                                 temp.body = variables.join('\n \t \t');
                             }
