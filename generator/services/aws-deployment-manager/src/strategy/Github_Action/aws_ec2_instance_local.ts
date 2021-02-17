9945453db5ba3bea53a3be288450fefce457aceb
manavalan <manavalanr619@gmail.com>
2021-02-17 21:38:01 +0530

    github actions for aws fargate is added

--- generator/services/aws-deployment-manager/src/strategy/Github_Action/aws_ec2_instance_local.ts
+++ generator/services/aws-deployment-manager/src/strategy/Github_Action/aws_ec2_instance_local.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { Request, response } from 'express';
 import * as util from 'util';
-import { Common } from '../config/Common';
-import { Constant } from '../config/Constant';
+import { Common } from '../../config/Common';
+import { Constant } from '../../config/Constant';
 
 
 export class Github_Actions_Local {
@@ -16,28 +16,41 @@ export class Github_Actions_Local {
 
 
     async create_ec2_instance_workflows_local(req: Request, callback: CallableFunction) {
+        const backendList = [];
         var projectDetails = req.body
         const project_name = projectDetails.project_unique_id
-        // check the github_action for root folder
-        // let check_folder = projectDetails.projectGenerationPath + `/${project_name}/`
-        // Common.removeFolders(check_folder + `/.github`)
+        const project_unique_id = projectDetails.project_unique_id.split("_");
+        projectDetails.customBackendList.forEach(backendElement => {
+            if (backendElement.isCustomCode) {
+                const temp = {
+                    name: '',
+                    port: '',
+                    uppername: '',
+                }
 
+                temp.name = backendElement.featureName;
+                temp.uppername = backendElement.featureName.toUpperCase();
+                temp.port = backendElement.nodePortNumber;
+                backendList.push(temp);
+            }
+        })
         let fileData = {
             project_name: projectDetails.name,
-            s3_bucket_name: project_name.replace(/_/g, '-')
+            s3_bucket_name: project_name.replace(/_/g, '-'),
+            project_unique_id: project_unique_id[1],
+            custom_features: backendList
+
         }
         // file path for local
-        let screenGenerationPath = projectDetails.projectGenerationPath + `/${project_name}/` + 'devops/cloud/aws/github_actions'
-        let remove_github = projectDetails.projectGenerationPath + `/${project_name}/` + '.github'
-        let templatePath = path.resolve(__dirname, '../../templates/github_actions');
+        let screenGenerationPath = projectDetails.projectGenerationPath + `/${project_name}/` + 'devops/cloud/aws/github_actions/workflows'
+        let templatePath = path.resolve(__dirname, '../../../templates/github_actions');
         let filePath = templatePath + `/ec2_instance_deployment.handlebars`;
-        let result: any = await this.handleBarsFile(filePath, fileData, screenGenerationPath, this.screenName, remove_github)
+        let result: any = await this.handleBarsFile(filePath, fileData, screenGenerationPath, this.screenName)
         callback(response)
     }
-    handleBarsFile(filePath, fileData, screenGenerationPath, screenName, remove_github) {
-        console.log("Going to the handler bars")
+    handleBarsFile(filePath, fileData, screenGenerationPath, screenName) {
         Handlebars.registerHelper('surroundWithCurlyBraces', function (text) {
-            var result = '{{' + text + '}}';
+            var result = '{{ ' + text + ' }}';
             return new Handlebars.SafeString(result);
         });
         Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
