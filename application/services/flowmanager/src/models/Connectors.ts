15d26e31fc3020fffbc605b15bf613f10090a845
manavalan <manavalanr619@gmail.com>
2021-04-26 21:42:46 +0530

    Added the requried model for custom connector

--- application/services/flowmanager/src/models/Connectors.ts
+++ application/services/flowmanager/src/models/Connectors.ts
@@ -10,11 +10,11 @@ export let connectorSchema = mongoose.Schema({
     description: String,
     apiMethods: String,
     entity_id: String,
-    entityName:String,  
+    entityName: String,
     api_key: {},
     service: String,
-    isQueryParams:Boolean,
-    endPointUrl:String,
+    isQueryParams: Boolean,
+    endPointUrl: String,
     connectorsType: String,
     availableApi: [
         {
@@ -22,6 +22,35 @@ export let connectorSchema = mongoose.Schema({
             description: { type: String, default: null },
             type: { type: String, default: null },
             properties: [],
+        }
+    ],
+    mls: [
+        {
+            name: { type: String, default: null },
+            auth: {type: String, default: false },
+            bearer: [{
+                key: { type: String, default: false },
+                value: { type: String, default: false },
+                type: { type: String, default: false },
+            }],
+            method: { type: String, default: false },
+            header: [{
+                type: String, default: false
+            }],
+            url: [{
+                raw: { type: String, default: null },
+                name: { type: String, default: null },
+                protocol: { type: String, default: null },
+                path: { type: String, default: null },
+                host: [{    
+                    raw: { type: Array, default: null },
+                }],
+                query: [{
+                    key: { type: String, default: null },
+                    value: { type: String, default: null },
+
+                }]
+            }],
         }
     ],
     url: {
@@ -43,6 +72,14 @@ export let connectorSchema = mongoose.Schema({
         default: false
     },
     properties: [],
+    type: {
+        type: String,
+        default: false
+    },
+    call_from: {
+        type: String,
+        default: false
+    },
     createdAt: {
         type: Date,
         default: Date.now
