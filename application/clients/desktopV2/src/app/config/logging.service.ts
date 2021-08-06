98fe267f2dfe73cb9b9ad9971b285820ebf4946c
Your Name <you@example.com>
2021-08-06 18:47:59 +0530

    angular 12 changes

---
+++ application/clients/desktopV2/src/app/config/logging.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { NGXLogger } from 'ngx-logger';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggingService {
+
+  constructor(public logger:NGXLogger) { }
+  log(level:string, message:string) {
+    switch (level) {
+      case 'debug' : {
+        this.logger.debug(message);
+        break;
+      }
+      case 'warning' : {
+        this.logger.warn(message);
+        break;
+      }
+      case 'info' : {
+        this.logger.info(message);
+        break;
+      }
+      case 'trace' : {
+       this.logger.trace(message);
+       break;
+     }
+      case 'error' : {
+        this.logger.error(message);
+        break;
+      }
+    }
+  }
+}
