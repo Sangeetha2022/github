1f4e8b95d1def3d2e2c1f3bb0e79d60213bd1f47
aharishsundhar <harish.s@10decoders.in>
2022-01-07 19:38:24 +0530

    to fix the base url for desktop to web and folder also template fixed

--- generator/services/Angular-gen-ManagerV10/src/config/Constant.ts
+++ generator/services/Angular-gen-ManagerV10/src/config/Constant.ts
@@ -38,7 +38,7 @@ export class Constant {
     public static AVAILABLE_CONNECTOR_NAME = `available`;
 
     // routes
-    public static DESKTOP_ROUTE = `desktop`;
+    public static WEB_ROUTE = `web`;
 
     // screen special events
     public static MODAL_SPECIALEVENT_NAME = `modal`;
@@ -113,7 +113,7 @@ export class Constant {
     public static POPUP_DATA_VARIABLENAME = 'data';
 
     // object name
-    public static DESKTOP_APINAME = 'DESKTOP_API';
+    public static WEB_APINAME = 'WEB_API';
     public static PROXY_CONFIG_VARIABLENAME = 'PROXY_CONFIG';
     public static NGINX_
 
