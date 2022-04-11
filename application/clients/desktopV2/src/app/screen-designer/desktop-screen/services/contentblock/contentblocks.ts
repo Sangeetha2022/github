80ccfe45f0e4f09cc1724f70b999ec747a5b2eec
aharishsundhar <harish.s@10decoders.in>
2022-04-11 20:40:55 +0530

    add sample side navigation blocks into check with generation side

---
+++ application/clients/desktopV2/src/app/screen-designer/desktop-screen/services/contentblock/contentblocks.ts
@@ -0,0 +1,292 @@
+export let contentblocks = `
+<nav id="template-i090p" class="">
+    <div id="i8k8l" class="container"></div>
+    <div id="ighf" class="gdp-row gpd-grid sticky">
+        <div id="ik48" class="cell gpd-clm">
+            <div id="ij0o8m" class="gdp-row gpd-grid">
+                <span onclick="openNav()" id="template-i2uji" class="">
+                    <div id="i8h3sg" class="cell gpd-clm">
+                        <img id="igjp1x" src="https://cdn.grapedrop.com/uf9ef8595751c4d1f87c91168f5d51a8c/8762113d99a84fa5baf2345306d4f0c3_hamburger3.png" />
+                    </div>
+                    </span>
+                <div id="ip9j7w" class="cell gpd-clm"><img id="i4dr2" src="https://cdn.grapedrop.com/uf9ef8595751c4d1f87c91168f5d51a8c/dad97ef59d704ea0b66e4e5416c67f0a_new_logo_gep.png" /></div>
+            </div>
+        </div>
+        <div id="mySidenav" class="sidenav">
+            <a [routerLink]="['javascript:void(0)']" onclick="closeNav()" id="template-if7ki" class="closebtn">
+                <div id="ibuiwl" class="cell gpd-clm">
+                    <img id="imvflv" src="https://cdn.grapedrop.com/uf9ef8595751c4d1f87c91168f5d51a8c/cdca723990474ba9ba33337d35ef399d_close.png" class="" />
+                </div>
+            </a>
+
+            <div id="MainMenu" class="">
+
+            </div>
+        </div>
+    </div>
+</nav>
+<script id='template-i29cx'>
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("foot").style.marginLeft = "250px";
+        document.getElementById("mySidenav").style.backgroundColor = "#1E1E1E";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("foot").style.marginLeft = "0%";
+    }
+</script>
+<style>
+    .gpd-container {
+      width: 90%;
+      padding-top: 25px;
+      padding-right: 0px;
+      padding-bottom: 25px;
+      padding-left: 0px;
+      margin-top: 0px;
+      margin-right: auto;
+      margin-bottom: 0px;
+      margin-left: auto;
+      max-width: 1200px;
+  }
+  .container {
+    width: 90%;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+}
+#ighf:hover {
+    background-color: rgb(30, 30, 30);
+}
+
+#ighf {
+    padding-top: 14px;
+    padding-right: 0px;
+    padding-bottom: 14px;
+    padding-left: 0px;
+    min-height: 70px;
+    background-color: rgb(30, 30, 30);
+    position: fixed;
+}
+.gdp-row {
+    display: flex;
+    justify-content: flex-start;
+    align-items: stretch;
+    flex-wrap: nowrap;
+}
+.gpd-container {
+    width: 90%;
+    padding-top: 25px;
+    padding-right: 0px;
+    padding-bottom: 25px;
+    padding-left: 0px;
+    margin-top: 0px;
+    margin-right: auto;
+    margin-bottom: 0px;
+    margin-left: auto;
+    max-width: 1200px;
+}
+
+.gpd-section {
+    display: flex;
+    padding-top: 50px;
+    padding-right: 0px;
+    padding-bottom: 50px;
+    padding-left: 0px;
+}
+
+.gpd-link {
+    display: inline-block;
+    text-decoration-line: none;
+    text-decoration-thickness: initial;
+    text-decoration-style: initial;
+    text-decoration-color: initial;
+    color: rgb(217, 131, 166);
+    vertical-align: top;
+    padding-top: 10px;
+    padding-right: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    max-width: 100%;
+}
+
+.gpd-text {
+    display: inline-block;
+    text-decoration-line: none;
+    text-decoration-thickness: initial;
+    text-decoration-style: initial;
+    text-decoration-color: initial;
+    color: inherit;
+    vertical-align: top;
+    padding-top: 10px;
+    padding-right: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    max-width: 100%;
+}
+.gpd-cnt {
+    padding-top: 0px;
+    padding-right: 0px;
+    padding-bottom: 0px;
+    padding-left: 0px;
+}
+
+.gpd-grid {
+    min-height: auto;
+    padding-top: 10px;
+    padding-right: 0px;
+    padding-bottom: 10px;
+    padding-left: 0px;
+}
+
+.gpd-clm {
+    padding-top: 5px;
+    padding-right: 0px;
+    padding-bottom: 5px;
+    padding-left: 0px;
+}
+
+.gpd-header {
+    margin-top: 0px;
+    margin-right: 0px;
+    margin-bottom: 0px;
+    margin-left: 0px;
+}
+
+.gpd-grid {
+    min-height: auto;
+    padding-top: 10px;
+    padding-right: 0px;
+    padding-bottom: 10px;
+    padding-left: 0px;
+}
+.sticky {
+    justify-content: space-between;
+    align-items: center;
+    min-height: 32px;
+    height: 60px;
+    max-width: 100%;
+    width: 100%;
+}
+#ik48 {
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 100%;
+    align-self: center;
+    padding-top: 5px;
+    padding-right: 0px;
+    padding-bottom: 5px;
+    padding-left: 16px;
+    white-space: normal;
+}
+.cell {
+    flex-grow: 1;
+    flex-basis: 100%;
+    padding-top: 5px;
+    padding-right: 5px;
+    padding-bottom: 5px;
+    padding-left: 5px;
+}
+#ij0o8m {
+    align-items: center;
+    padding-top: 0px;
+    padding-right: 0px;
+    padding-bottom: 0px;
+    padding-left: 0px;
+}
+#i8h3sg {
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+    padding-top: 8px;
+    padding-right: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    background-color: rgb(97, 97, 107);
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border-bottom-left-radius: 4px;
+    max-width: 48px;
+}
+#igjp1x {
+    color: black;
+}
+#ip9j7w {
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+    margin-top: 0px;
+    margin-right: 0px;
+    margin-bottom: 0px;
+    margin-left: 15px;
+    padding-top: 0px;
+    padding-right: 0px;
+    padding-bottom: 0px;
+    padding-left: 0px;
+}
+#i4dr2 {
+    color: black;
+    max-width: 58%;
+}
+.sidenav .closebtn {
+    color: white;
+    position: absolute;
+    top: -18px;
+    right: 0px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+.sidenav {
+    width: 0px;
+    position: fixed;
+    z-index: 1;
+    top: 69px;
+    left: 0px;
+    background-color: rgba(0, 0, 0, 0.5);
+    overflow-x: hidden;
+    transition-duration: 0.5s;
+    transition-timing-function: ease;
+    transition-delay: 0s;
+    transition-property: all;
+    font-family: "Open Sans", sans-serif;
+}
+
+.sidenav a {
+    text-decoration-line: none;
+    text-decoration-style: initial;
+    text-decoration-color: initial;
+    color: rgb(255, 255, 255);
+    display: block;
+    transition-duration: 0.3s;
+    transition-timing-function: ease;
+    transition-delay: 0s;
+    transition-property: all;
+    background-color: transparent;
+    font-size: 20px;
+}
+#ibuiwl {
+    text-align: right;
+    padding-top: 15px;
+    padding-right: 14px;
+    padding-bottom: 5px;
+    padding-left: 0px;
+}
+#imvflv {
+    color: black;
+}
+@media (max-width: 768px) {
+    .gdp-row {
+        flex-wrap: wrap;
+    }
+}
+@media screen and (max-height: 450px) {
+    .sidenav a {
+        font-size: 18px;
+    }
+}
+</style>
+`
